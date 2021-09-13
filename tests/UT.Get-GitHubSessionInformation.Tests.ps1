f8446e8cc4aa9415f4aef4345c60884d33c46260
NickGraham101 <nick.graham101@googlemail.com>
2021-09-13 14:49:00 +0100

    Added test for Get-GitHubSessionInformation

---
+++ tests/UT.Get-GitHubSessionInformation.Tests.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module $PSScriptRoot\..\src\GitHubToolKit.psm1 -Force
+
+Describe "Get-GitHubSessionInformation tests" -Tags @("Unit") {
+    Mock Write-Output -ModuleName GitHubToolKit
+
+    Context "No session information has been set" {
+        It "Should not call Write-Output" {
+                $null = Get-GitHubSessionInformation
+                Assert-MockCalled -CommandName Write-Output -ModuleName GitHubToolKit -Times 0
+        }
+    }
+
+    Context "Session information has been set" {
+        It "Should call Write-Output once" {
+                Set-GitHubSessionInformation -PatToken "not-a-real-pat-token"
+                $null = Get-GitHubSessionInformation
+                Assert-MockCalled -CommandName Write-Output -ModuleName GitHubToolKit -Times 1
+        }
+    }
+}
