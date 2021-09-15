a358c70a335aac201de4921b872aa170f24e989c
NickGraham101 <nick.graham101@googlemail.com>
2021-09-15 10:20:51 +0100

    Added tests for Remove-GitHubSessionInformation

---
+++ tests/UT.Remove-GitHubSessionInformation.Tests.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module $PSScriptRoot\..\src\GitHubToolKit.psm1 -Force
+
+#Skipped tests: Remove-GitHubSessionInformation implements SupportsShouldProcess,ConfirmImpact="High" forcing an unnecessary manual confirmation which also prevents testing
+Describe "Remove-GitHubSessionInformation tests" -Tags @("Unit") {
+    Mock Remove-Variable -ModuleName GitHubToolKit
+    
+    Context "GitHubSessionInformation has not been set" {
+        It "Should call Remove-Variable successfully" -Skip {
+            Remove-GitHubSessionInformation
+        }
+    }
+
+    Context "GitHubSessionInformation has been set" {
+        It "Should call Remove-Variable successfully" -Skip {
+            Set-GitHubSessionInformation -PatToken "not-a-real-pat-token"
+            Remove-GitHubSessionInformation
+        }
+    }
+}
