c43d91384a9974f470a9e1b5916ab858d058535b
NickGraham101 <nick.graham101@googlemail.com>
2021-09-15 16:49:54 +0100

    Added Get-GitHubRepos tests

---
+++ tests/UT.Get-GitHubRepos.Tests.ps1
@@ -0,0 +1,26 @@
+﻿Import-Module $PSScriptRoot\..\src\GitHubToolKit.psm1 -Force
+
+Describe "Get-GitHubRepos tests" -Tags @("Unit") {
+
+    Context "The GitHub API returns a valid, single page response" {
+        Mock Invoke-RestMethod -ModuleName GitHubToolKit -MockWith {
+            $Nodes = @(
+                New-Object -TypeName PSCustomObject -Property @{ name = "foo-bar-repo" }
+                New-Object -TypeName PSCustomObject -Property @{ name = "foo-foo-repo" }
+            )
+            $PageInfo = New-Object -TypeName PSCustomObject -Property @{ endCursor = "nOTarEALcURSOR=="; hasNextPage = "False" }
+            $Repositories = New-Object -TypeName PSCustomObject -Property @{ nodes = $Nodes; pageInfo = $PageInfo }
+            $Organization = New-Object -TypeName PSCustomObject -Property @{ repositories = $Repositories }
+            $Data = New-Object -TypeName PSCustomObject -Property @{ organization = $Organization }
+            $ReturnObject = New-Object -TypeName PSCustomObject -Property @{ data = $Data }
+            return $ReturnObject
+        }
+        
+        It "Should call Invoke-RestMethod once and return an " {
+            Set-GitHubSessionInformation -PatToken "not-a-real-pat-token"
+            $Result = Get-GitHubRepos -GitHubOrg SkillsFundingAgency
+            $Result.Count | Should -Be 2
+            Assert-MockCalled -CommandName Invoke-RestMethod -ModuleName GitHubToolKit -Times 1 -Exactly
+        }
+    }
+}
