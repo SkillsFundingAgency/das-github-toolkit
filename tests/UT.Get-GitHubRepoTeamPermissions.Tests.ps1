36c85981a65418a9c051b97f45bf2add227d6ed9
NickGraham101 <nick.graham101@googlemail.com>
2021-09-20 15:34:38 +0100

    Added test for Get-GithubRepoTeamPermissions

---
+++ tests/UT.Get-GitHubRepoTeamPermissions.Tests.ps1
@@ -0,0 +1,39 @@
+﻿Import-Module $PSScriptRoot\..\src\GitHubToolKit.psm1 -Force
+
+Describe "Get-GithubRepoTeamPermissions tests" -Tags @("Unit") {
+
+    Context "The GitHub API returns a valid response" {
+        Mock Invoke-GitHubRestMethod -ModuleName GitHubToolKit -ParameterFilter { $Uri -match "\/orgs\/.*\/repos" } -MockWith {
+            @(
+                @{
+                    id = 12345678
+                    name = "foo-bar-repo"
+                },
+                @{
+                    id = 87654321
+                    name = "foo-foo-repo"
+                }
+            )
+        }
+
+        Mock Invoke-GitHubRestMethod -ModuleName GitHubToolKit -ParameterFilter { $Uri -match "\/repos\/.*\/.*\/teams" } -MockWith {
+            @(
+                @{
+                    id = 12345678
+                    name = "foo-contributor"
+                },
+                @{
+                    id = 87654321
+                    name = "foo-admin"
+                }
+            )
+        }
+        
+        It "Should return an array of hashtables containing repo teams" {
+            Set-GitHubSessionInformation -PatToken "not-a-real-pat-token"
+            $Result = Get-GithubRepoTeamPermissions
+            $Result.Count | Should -Be 2
+            { $Result | Select-Object -ExpandProperty teams | Should -Not -Throw }
+        }
+    }
+}
