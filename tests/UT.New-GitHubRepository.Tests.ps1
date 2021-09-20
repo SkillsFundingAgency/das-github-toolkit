0beee4a4de3390b59d700e1c1efd026948f7d4d9
NickGraham101 <nick.graham101@googlemail.com>
2021-09-20 14:34:27 +0100

    Added test for New-GitHubRepository

---
+++ tests/UT.New-GitHubRepository.Tests.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module $PSScriptRoot\..\src\GitHubToolKit.psm1 -Force
+
+Describe "New-GitHubRepository tests" -Tags @("Unit") {
+
+    Context "All mandatory parameters are passed in" {
+        
+        #Skipped tests: uses deprecated method to authenticate with GitHub API that's hardcoded into the script
+        It "Should call Invoke-RestMethod using POST on /user/repos" -Skip {
+
+        }
+    }
+}
