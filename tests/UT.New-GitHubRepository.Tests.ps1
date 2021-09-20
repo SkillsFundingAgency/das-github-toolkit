2db1ea2f70b5bdd21368cfd8dd2e79c400d9be91
NickGraham101 <nick.graham101@googlemail.com>
2021-09-20 14:46:38 +0100

    Corrections for New-GitHubRepository tests

--- tests/UT.New-GitHubRepository.Tests.ps1
+++ tests/UT.New-GitHubRepository.Tests.ps1
@@ -3,9 +3,10 @@
 Describe "New-GitHubRepository tests" -Tags @("Unit") {
 
     Context "All mandatory parameters are passed in" {
-        
+        Mock Invoke-GitHubRestMethod
+
         #Skipped tests: uses deprecated method to authenticate with GitHub API that's hardcoded into the script
-        It "Should call Invoke-RestMethod using POST on /user/repos" -Skip {
+        It "Should call Invoke-GitHubRestMethod using POST on /user/repos" -Skip {
 
         }
     }
