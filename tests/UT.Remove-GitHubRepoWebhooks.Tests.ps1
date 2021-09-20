f1f77beaef60981cefec31ddd792af030d150097
NickGraham101 <nick.graham101@googlemail.com>
2021-09-20 14:55:57 +0100

    Added test for Remove-GitHubRepoWebhooks

---
+++ tests/UT.Remove-GitHubRepoWebhooks.Tests.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module $PSScriptRoot\..\src\GitHubToolKit.psm1 -Force
+
+Describe "Remove-GitHubRepoWebhooks tests" -Tags @("Unit") {
+
+    Context "All mandatory parameters are passed in" {
+        Mock Invoke-GitHubRestMethod
+
+        #Skipped tests: uses deprecated method to authenticate with GitHub API that's hardcoded into the script
+        It "Should call Invoke-GitHubRestMethod using DELETE on /repos/{Organization}/{Repo Name}/hooks/{Hook id}" -Skip {
+
+        }
+    }
+}
