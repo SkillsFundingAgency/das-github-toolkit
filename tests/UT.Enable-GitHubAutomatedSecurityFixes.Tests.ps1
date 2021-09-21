c858133810b3f1395708fc438e09860b8cea62d1
NickGraham101 <nick.graham101@googlemail.com>
2021-09-21 13:38:31 +0100

    Added test for Enable-GitHubAutomatedSecurityFixes

---
+++ tests/UT.Enable-GitHubAutomatedSecurityFixes.Tests.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module $PSScriptRoot\..\src\GitHubToolKit.psm1 -Force
+
+Describe "Enable-GitHubAutomatedSecurityFixes tests" -Tags @("Unit") {
+    #Mock Get-GithubOrgPublicRepos
+    Mock Invoke-WebRequest
+
+    Context "All parameters are passed in" {
+        #Skipped tests: calls Get-GithubOrgPublicRepos.ps1 as a script rather than a function so it can't be mocked
+        It "Should call Invoke-WebRequest once for each repo" -Skip {
+
+        }
+    }
+}
