f6019e49243747a2ad9b06edf2193e87d048c77b
NickGraham101 <nick.graham101@googlemail.com>
2021-09-22 16:23:15 +0100

    Added integration test for Get-GitHubRepoTeamPermission

---
+++ tests/IT.Get-GitHubRepoTeamPermission.Tests.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module $PSScriptRoot\..\src\GitHubToolKit.psm1 -Force
+
+Describe "Get-GitHubRepoTeamPermission integration tests" -Tags @("Integration") {
+    . $PSScriptRoot\IT.Mocks.ps1
+    
+    Context "The GitHub API returns a valid response" {
+        It "Should return an array of hashtables containing repo teams" {
+            Set-GitHubSessionInformation -PatToken $Env:GitHubPatToken
+            $Result = Get-GitHubRepoTeamPermission
+            $Result.Count | Should -BeGreaterThan 1
+            { $Result | Select-Object -ExpandProperty teams | Should -Not -Throw }
+        }
+    }
+}
---
+++ tests/IT.Mocks.ps1
@@ -0,0 +1,3 @@
+﻿# Mock Invoke-RestMethod and Invoke-WebRequest cmdlets with any Method parameter value other than Get to prevent potentially destructive API calls during tests
+Mock Invoke-RestMethod -ModuleName GitHubToolKit -ParameterFilter { $Method -notmatch "get" }
+Mock Invoke-WebRequest -ModuleName GitHubToolKit -ParameterFilter { $Method -notmatch "get" }
