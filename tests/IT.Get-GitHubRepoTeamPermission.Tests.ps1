ab01e5d4dde0da9da205ab73707a3e52de709924
NickGraham101 <nick.graham101@googlemail.com>
2021-09-23 09:01:02 +0100

    Added logging to integration test

--- tests/IT.Get-GitHubRepoTeamPermission.Tests.ps1
+++ tests/IT.Get-GitHubRepoTeamPermission.Tests.ps1
@@ -5,8 +5,10 @@ Describe "Get-GitHubRepoTeamPermission integration tests" -Tags @("Integration")
 
     Context "The GitHub API returns a valid response" {
         It "Should return an array of hashtables containing repo teams" {
+            ##TO DO: remove Write-Host
+            Write-Host $Env:GitHubPatToken
             Set-GitHubSessionInformation -PatToken $Env:GitHubPatToken
-            $Result = Get-GitHubRepoTeamPermission
+            $Result = Get-GitHubRepoTeamPermission -Verbose #TO DO: remove -Verbose
             $Result.Count | Should -BeGreaterThan 1
             { $Result | Select-Object -ExpandProperty teams | Should -Not -Throw }
         }
