d38eeb2473dd6cb491fe5f2cb658060560ed469f
NickGraham101 <nick.graham101@googlemail.com>
2021-09-23 09:29:38 +0100

    Changed casing on mapped variable

--- tests/IT.Get-GitHubRepoTeamPermission.Tests.ps1
+++ tests/IT.Get-GitHubRepoTeamPermission.Tests.ps1
@@ -6,8 +6,8 @@ Describe "Get-GitHubRepoTeamPermission integration tests" -Tags @("Integration")
     Context "The GitHub API returns a valid response" {
         It "Should return an array of hashtables containing repo teams" {
             ##TO DO: remove Write-Host
-            Write-Host "Value: $($Env:GitHubPatToken.Substring(0,40))"
-            Set-GitHubSessionInformation -PatToken $Env:GitHubPatToken
+            Write-Host "Value: $($env:GITHUBPATTOKEN.Substring(0,40))"
+            Set-GitHubSessionInformation -PatToken $env:GITHUBPATTOKEN
             $Result = Get-GitHubRepoTeamPermission -Verbose #TO DO: remove -Verbose
             $Result.Count | Should -BeGreaterThan 1
             { $Result | Select-Object -ExpandProperty teams | Should -Not -Throw }
