af5bba739792344adaa24b23b32f25a994ff4b02
NickGraham101 <nick.graham101@googlemail.com>
2021-09-23 10:51:42 +0100

    Added additional integration test

--- tests/IT.Get-GitHubRepoTeamPermission.Tests.ps1
+++ tests/IT.Get-GitHubRepoTeamPermission.Tests.ps1
@@ -5,12 +5,24 @@ Describe "Get-GitHubRepoTeamPermission integration tests" -Tags @("Integration")
 
     Context "The GitHub API returns a valid response" {
         It "Should return an array of hashtables containing repo teams" {
-            ##TO DO: remove Write-Host
-            Write-Host "Value: $($env:GITHUBPATTOKEN.Substring(0,40))"
             Set-GitHubSessionInformation -PatToken $env:GITHUBPATTOKEN
             $Result = Get-GitHubRepoTeamPermission -Verbose #TO DO: remove -Verbose
             $Result.Count | Should -BeGreaterThan 1
             { $Result | Select-Object -ExpandProperty teams | Should -Not -Throw }
+        }
+    }
+
+    Context "The GitHub API returns a valid response" {
+        It "Should return an array of hashtables containing repo teams without any warnings" {
+            Mock -CommandName Write-Warning -ModuleName GitHubToolKit
+            Mock -CommandName Write-Error -ModuleName GitHubToolKit
+
+            Set-GitHubSessionInformation -PatToken $env:GITHUBPATTOKEN
+            $Result = Get-GitHubRepoTeamPermission
+            $Result.Count | Should -BeGreaterThan 1
+            { $Result | Select-Object -ExpandProperty teams | Should -Not -Throw }
+            Assert-MockCalled -CommandName Write-Warning -ModuleName GitHubToolKit -Times 0 -Exactly
+            Assert-MockCalled -CommandName Write-Error -ModuleName GitHubToolKit -Times 0 -Exactly
         }
     }
 }
